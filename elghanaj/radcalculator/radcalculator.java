838971b632dbcda193779855ae6187c14ce6de34
Joseph El-Ghaname <elghanaj@uwindsor.ca>
2020-02-18 23:00:11 -0500

    Update elghanaj/radcalculator/radcalculator.java, elghanaj/radcalculator/radcalculatortest.java files

---
+++ elghanaj/radcalculator/radcalculator.java
@@ -0,0 +1,10 @@
+
+public class RadCalculator {
+	    public int add(int a, int b) {
+	        return a + b;
+	    }
+	     
+	    public int subtract(int a, int b) {
+	        return a - b;
+	    }
+}
