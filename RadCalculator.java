3ffa751032f1c28a6188d586c53fcf67d955dadf
Joseph El-Ghaname <elghanaj@uwindsor.ca>
2020-02-22 12:53:13 -0500

    v1.4

--- RadCalculator.java
+++ RadCalculator.java
@@ -1,5 +1,6 @@
 public class RadCalculator {
 
+	// Power function used to calculate taylor series of sin(x), cos(x), and tan(x) formulas
 	public static double power(double a, double b) {
 		double result = a;
 		for(int i = 1; i < b; i++) {
@@ -8,17 +9,21 @@ public class RadCalculator {
 		return result;
 	}
 
+	// Factorial function used to calculate taylor series of sin(x) and cos(x) formulas 
 	public static double factorial(double a) {
 		double result = a;
-
 		for(double i = a - 1; i > 0; i--) {
 			result *= i;
 		}
 		return result;
 	}
 
+    // Implementation of sin(x)
+
 	public static double sin(double a) {
 		double result = a; 
+		
+		// Taylor series implemenation for sin(x)
 		int sign = -1;
 		for(int i = 3; i < 500; i = i + 2) {
 			result += (sign *((power(a,i)) / factorial(i)));
@@ -28,8 +33,12 @@ public class RadCalculator {
         return result;
     }
 
+    // Implementation of cos(x)
+	
     public static double cos(double a) {
-		double result = 1; 
+		double result = 1;
+		
+		// Taylor series implementation for cos(x)
 		int sign = -1;
 		for(int i = 2; i < 500; i = i + 2) {
 			result += (sign *((power(a,i)) / factorial(i)));
@@ -39,8 +48,11 @@ public class RadCalculator {
         return result;
     }
 
+    // Implementation of tan(x)
+
     public static double tan(double a) {
 
+    	// Taylor series implementation for tan(x)
 		double result = a + (power(a, 3)/3) + ((2 * power(a, 5))/15) + ((17 * power(a, 7))/315) + ((62 * power(a, 9))/2835);
 		
         return result;
--- RadCalculatorTest.java
+++ RadCalculatorTest.java
@@ -7,48 +7,63 @@ import java.math.RoundingMode;
 
 class RadCalculatorTest {
 
+	
+	/*
+	Test Case #1: Testing the sin(x) implementation with maximum accuracy
+	*/
     @Test
     public void testsin() {
         RadCalculator testSinObject = new RadCalculator();
         
-        double a = 1.0;
+        double a = 1.0; // test case
         
-        double testAnswer = Math.sin(a);
+        double testAnswer = Math.sin(a); // real value of test case
         
-        double result = testSinObject.sin(a);
+        double result = testSinObject.sin(a); // real result from implementation
         
-        assertEquals(testAnswer, result);
+        assertEquals(testAnswer, result); // comparing test case with real result
     }
      
+	/*
+	Test Case #2: Testing the cos(x) implementation with limited accuracy
+	*/
     @Test
     public void testcos() {
     	RadCalculator testCosObject = new RadCalculator();
         
-        double a = 1.0;
+        double a = 1.0; // test case
         
-        double testAnswer = Math.cos(a);
+        double testAnswer = Math.cos(a); // real value of test case
         
-        double result = testCosObject.cos(a);
+        double result = testCosObject.cos(a); // real result from implementation
+        
+        // Scaling accuracy to suit the test case
         
         BigDecimal testAnswer1 = new BigDecimal (testAnswer).setScale(15, RoundingMode.DOWN);
 		
-		testAnswer = (double) testAnswer1.doubleValue();
+		testAnswer = (double) testAnswer1.doubleValue(); 
 		
 		BigDecimal result1 = new BigDecimal (result).setScale(15, RoundingMode.DOWN);
 		
-		result = (double) result1.doubleValue();
+		result = (double) result1.doubleValue(); 
         
-        assertEquals(testAnswer, result);
+        assertEquals(testAnswer, result); // comparing test case with real result
     }
+    
+	/*
+	Test Case #3: Testing the tan(x) implementation with limited accuracy
+	*/
     @Test
     public void testtan() {
     	RadCalculator testTanObject = new RadCalculator();
         
-        double a = 1.0;
+        double a = 1.0; // test case
         
-        double testAnswer = Math.tan(a);
+        double testAnswer = Math.tan(a); // real value of test case
         
-        double result = testTanObject.tan(a);
+        double result = testTanObject.tan(a); // real result from implementation
+        
+        // Scaling accuracy to suit the test case
         
         BigDecimal testAnswer1 = new BigDecimal (testAnswer).setScale(1, RoundingMode.DOWN);
 		
@@ -56,9 +71,9 @@ class RadCalculatorTest {
 		
 		BigDecimal result1 = new BigDecimal (result).setScale(1, RoundingMode.DOWN);
 		
-		result = (double) result1.doubleValue();
+		result = (double) result1.doubleValue(); 
         
-        assertEquals(testAnswer, result);
+        assertEquals(testAnswer, result); // comparing test case with real result
     }
    
 
